51c60c666098d2e16b3d903d4c35cc63c754bd64
Edionay <edionay@gmail.com>
2018-06-25 18:19:15 -0300

    Corrigindo nome da pasta docs.

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Consumidor","l":"Consumidor()"},{"p":"<Unnamed>","c":"Consumidor","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"Publicador","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"Publicador","l":"Publicador()"}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Consumidor"},{"p":"<Unnamed>","l":"Publicador"}]
