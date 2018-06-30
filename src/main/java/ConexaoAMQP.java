c954ce4d54f4107f08a31b54da562e01235c24bf
Edionay <edionay@gmail.com>
2018-06-30 02:03:14 -0300

    Implementação do método consumigMensagem() na Classe ConexaoAMQP.

--- src/main/java/ConexaoAMQP.java
+++ src/main/java/ConexaoAMQP.java
@@ -1,9 +1,11 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
@@ -23,5 +25,21 @@ public class ConexaoAMQP  {
         canal.basicPublish("", "pedidos", null, mensagem.getBytes());
     }
 
+    public void consumirMensagem() throws IOException, InterruptedException {
+        QueueingConsumer consumidor = new QueueingConsumer(canal);
+        canal.basicConsume("pedidos", false, consumidor);
+
+        QueueingConsumer.Delivery entrega = consumidor.nextDelivery();
+
+        if (entrega != null) {
+            try {
+                String mensagem = new String(entrega.getBody(), StandardCharsets.UTF_8);
+                System.out.println("Mensagem consumida: " +mensagem);
+                canal.basicAck(entrega.getEnvelope().getDeliveryTag(), false);
+            } catch (Exception e) {
+                canal.basicAck(entrega.getEnvelope().getDeliveryTag(), true);
+            }
+        }
+    }
 }
 
