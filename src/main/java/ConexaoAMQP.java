52cdccd5d6458270b23c47cae0a346582fea013d
Edionay <edionay@gmail.com>
2018-06-30 01:26:05 -0300

    Criação da classe ConexaoAMQP.

--- src/main/java/ConexaoAMQP.java
+++ src/main/java/ConexaoAMQP.java
@@ -1,40 +1,27 @@
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeoutException;
 
-public class Consumidor {
-    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException, InterruptedException {
-        ConnectionFactory fabricaDeConexao = new ConnectionFactory();
-        fabricaDeConexao.setUri("amqp://guest:guest@localhost");
-        fabricaDeConexao.setConnectionTimeout(300000);
-        Connection connection = fabricaDeConexao.newConnection();
-        Channel canal = connection.createChannel();
+public class ConexaoAMQP  {
 
-        canal.queueDeclare("lista", true, false, false, null);
+    private ConnectionFactory fabricaDeConexao = new ConnectionFactory();
+    private Connection conexao = fabricaDeConexao.newConnection();
+    private Channel canal = conexao.createChannel();
 
-        QueueingConsumer consumidor = new QueueingConsumer(canal);
-        canal.basicConsume("lista", false, consumidor);
-
-        while (true) {
-            QueueingConsumer.Delivery entrega = consumidor.nextDelivery();
-
-            if (entrega != null) {
-                try {
-                    String mensagem = new String(entrega.getBody(), StandardCharsets.UTF_8);
-                    System.out.println("Mensagem consumida: " +mensagem);
-                    canal.basicAck(entrega.getEnvelope().getDeliveryTag(), false);
-                } catch (Exception e) {
-                    canal.basicAck(entrega.getEnvelope().getDeliveryTag(), true);
-
-                }
-            }
-        }
-
-
+    public ConexaoAMQP(String enderecoAMQP, String fila) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException {
+        fabricaDeConexao.setUri(enderecoAMQP);
+        fabricaDeConexao.setConnectionTimeout(10000);
+        canal.queueDeclare(fila, true, false, false, null);
     }
+
+    public void enviarMensagem(String mensagem) throws IOException {
+        canal.basicPublish("", "pedidos", null, mensagem.getBytes());
+    }
+
 }
+
