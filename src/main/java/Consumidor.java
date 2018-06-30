1ba3662bb0feec80027a889914e5f4def75d133f
Edionay <edionay@gmail.com>
2018-06-30 02:04:49 -0300

    Consumindo a Classe ConexaoAMQP pelo Consumidor.

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -9,32 +9,8 @@ import java.util.concurrent.TimeoutException;
 
 public class Consumidor {
     public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException, InterruptedException {
-        ConnectionFactory fabricaDeConexao = new ConnectionFactory();
-        fabricaDeConexao.setUri("amqp://guest:guest@localhost");
-        fabricaDeConexao.setConnectionTimeout(300000);
-        Connection connection = fabricaDeConexao.newConnection();
-        Channel canal = connection.createChannel();
 
-        canal.queueDeclare("pedidos", true, false, false, null);
-
-        QueueingConsumer consumidor = new QueueingConsumer(canal);
-        canal.basicConsume("pedidos", false, consumidor);
-
-        while (true) {
-            QueueingConsumer.Delivery entrega = consumidor.nextDelivery();
-
-            if (entrega != null) {
-                try {
-                    String mensagem = new String(entrega.getBody(), StandardCharsets.UTF_8);
-                    System.out.println("Mensagem consumida: " +mensagem);
-                    canal.basicAck(entrega.getEnvelope().getDeliveryTag(), false);
-                } catch (Exception e) {
-                    canal.basicAck(entrega.getEnvelope().getDeliveryTag(), true);
-
-                }
-            }
-        }
-
-
+        ConexaoAMQP conexao = new ConexaoAMQP("amqp://guest:guest@localhost", "pedidos");
+        conexao.consumirMensagem();
     }
 }
