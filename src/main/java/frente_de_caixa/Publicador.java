1509e57360a323919b2b190181f176fa2d5f5657
Edionay <edionay@gmail.com>
2018-07-02 16:48:23 -0300

    Excluindo loop de publicação de mensagem.

--- src/main/java/frente_de_caixa/Publicador.java
+++ src/main/java/frente_de_caixa/Publicador.java
@@ -15,23 +15,11 @@ public class Publicador {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException, TimeoutException {
 
-        ConexaoAMQP conexao = new ConexaoAMQP("amqp://guest:guest@localhost", "pedidos");
-
         Venda venda = new Venda("Sabonete",2.50,10);
 
-        int contador = 0;
-
         Random random = new Random();
-        while (contador < 5000) {
-            venda.setQuantidade(random.nextInt(100));
-            String mensagem = "Mensagem número: " + contador;
-            publicaMensagem(venda,"pedidos");
-
-            contador++;
-            System.out.println("Mensagem publicada: " + mensagem);
-
-            Thread.sleep(4000);
-        }
+        venda.setQuantidade(random.nextInt(100));
+        publicaMensagem(venda,"pedidos");
 
     }
 
