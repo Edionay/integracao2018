b8855bc3c1170155dab5bf3412a84a55b7a57753
Edionay <edionay@gmail.com>
2018-07-03 00:09:11 -0300

    Utilizando o novo serializador.

--- src/main/java/frente_de_caixa/Publicador.java
+++ src/main/java/frente_de_caixa/Publicador.java
@@ -1,14 +1,12 @@
 package frente_de_caixa;
 
 import conexao_amqp.ConexaoAMQP;
-import serializer.Serializer;
 import venda.Venda;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 public class Publicador {
@@ -16,9 +14,6 @@ public class Publicador {
     public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException, TimeoutException {
 
         Venda venda = new Venda("Sabonete",2.50,10);
-
-        Random random = new Random();
-        venda.setQuantidade(random.nextInt(100));
         publicaMensagem(venda,"pedidos");
 
     }
@@ -34,10 +29,9 @@ public class Publicador {
      */
     public static void publicaMensagem(Object objeto, String fila) throws IOException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
 
-        ConexaoAMQP conexao = new ConexaoAMQP("amqp://guest:guest@localhost", fila);
-        Serializer serializer = new Serializer();
+        ConexaoAMQP conexao = new ConexaoAMQP("amqp://guest:guest@localhost");
 
         //Serializa o objeto e publica na fila em byte[]
-        conexao.enviarMensagem(serializer.serialize(objeto),fila);
-    }
+        conexao.enviarMensagem(Serializador.paraJSON(objeto),fila);
+        }
 }
