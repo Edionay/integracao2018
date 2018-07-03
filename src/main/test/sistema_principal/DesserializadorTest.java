ecf95e016457286320e8c578d3089515894ec3bf
Edionay <edionay@gmail.com>
2018-07-03 00:07:04 -0300

    Criação da classe Desserializador usada pelo Consumidor.

--- src/main/test/sistema_principal/DesserializadorTest.java
+++ src/main/test/sistema_principal/DesserializadorTest.java
@@ -1,5 +1,20 @@
+package sistema_principal;
+
+import com.google.gson.Gson;
+import org.junit.Test;
+import venda.Venda;
+
 import static org.junit.Assert.*;
+import static sistema_principal.Desserializador.jsonParaVenda;
 
 public class DesserializadorTest {
 
+    @Test
+    public void testJsonParaVenda() {
+
+        Venda venda = new Venda("Sabonete",2.50,10);
+
+        String json = "{\"produto\":\"Sabonete\",\"valor\":2.5,\"quantidade\":10 }";
+        assertEquals(venda, jsonParaVenda(json));
+    }
 }
