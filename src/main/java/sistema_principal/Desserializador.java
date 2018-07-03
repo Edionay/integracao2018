c9c64f1fc024ea38347ad3db06d01bd1b66a7f68
Edionay <edionay@gmail.com>
2018-07-03 00:08:02 -0300

    Criação da classe Serializador utilizada pelo Puclicador.

--- src/main/java/sistema_principal/Desserializador.java
+++ src/main/java/sistema_principal/Desserializador.java
@@ -1,4 +1,18 @@
 package sistema_principal;
 
-public class Serializador {
+import com.google.gson.Gson;
+import venda.Venda;
+
+public class Desserializador {
+    /**
+     * Serializa um JSON para um objeto Venda
+     * @param mensagem
+     * @return Objeto Venda
+     */
+    public static Venda jsonParaVenda(String mensagem) {
+
+        Gson json = new Gson();
+
+        return json.fromJson(mensagem, Venda.class);
+    }
 }
